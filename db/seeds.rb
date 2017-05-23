7c32ae067005f945b61cbd8d1c9c869728f7acda
Danick <danick.sikkema@gmail.com>
2017-05-23 09:40:01 +0200

    Changed type to integer

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 
 5.times do
   user = User.create({name: Faker::Name.name})
-  user.permits.create({permitName: Faker::Name.name, permitType: Faker::Lorem.sentence})
+  user.permits.create({permitName: Faker::Name.name, permitType: 1})
 end
