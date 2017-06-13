31e36e5e535ad7f0917e89ff4071310a0563acfa
Danick <danick.sikkema@gmail.com>
2017-06-13 13:24:55 +0200

    created migration for protections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522175621) do
+ActiveRecord::Schema.define(version: 20170613111902) do
 
   create_table "permits", force: :cascade do |t|
     t.string   "permitName"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20170522175621) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.index ["user_id"], name: "index_permits_on_user_id"
+  end
+
+  create_table "protections", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "permit_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["permit_id"], name: "index_protections_on_permit_id"
   end
 
   create_table "users", force: :cascade do |t|
