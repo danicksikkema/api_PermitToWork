b57bfd727e4d9deec2869c1a00f1904d558cb2cc
Danick <danick.sikkema@gmail.com>
2017-06-13 14:31:30 +0200

    added back relations

--- app/models/permit.rb
+++ app/models/permit.rb
@@ -2,7 +2,7 @@ class Permit < ApplicationRecord
   validates :permitName, presence: true
   validates :permitType, presence: true
 
-  # has_many :precautions
-  # has_many :protections
-  # has_many :environments
+  has_many :precautions
+  has_many :protections
+  has_many :environments
 end
