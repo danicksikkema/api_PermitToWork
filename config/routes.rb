3868e784c717db1705482867e31db857e31a6ca3
Danick <danick.sikkema@gmail.com>
2017-06-13 13:15:58 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :permits
+      resources :permits do
+        resources :environments
+        resources :precautions
+        resources :protections
+      end
       resources :users
     end
   end
