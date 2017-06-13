90194e42bd336a8f82a6534197ace96eb9842807
Danick <danick.sikkema@gmail.com>
2017-06-13 15:37:06 +0200

    fix

--- app/controllers/api/v1/protections_controller.rb
+++ app/controllers/api/v1/protections_controller.rb
@@ -1,50 +1,50 @@
 module Api
     module V1
-        class ProtectionsController < ApplicationController
-          def index
-            @protections = Protection.order('created_at DESC')
-            render json: @protections
-          end
+      class ProtectionsController < ApplicationController
+        def index
+          @protections = Protection.order('created_at DESC')
+          render json: @protections
+        end
 
-          def show
-            @protection = Protection.find(params[:id])
-            render json: @protection
-          end
+        def show
+          @protection = Protection.find(params[:id])
+          render json: @protection
+        end
 
-          def create
-            @protection = Protection.new(protection_params)
+        def create
+          @protection = Protection.new(protection_params)
 
-            if @protection.save
-              render json: @protection, status: :created
-            else
-              render json: @protection.errors, status: :unprocessable_entity
-            end
-          end
-
-          def update
-            @protection = Protection.find(params[:id])
-
-            if @protection.update(protection_params)
-              render json: @protection, status: :update
-            else
-              render json: @protection.errors, status: :unprocessable_entity
-            end
-          end
-
-          def destroy
-            @protection = Protection.find(params[:id])
-            if @protection
-              @protection.destroy
-            else
-              render json: {post: "not found"}, status: :not_found
-            end
-          end
-
-          private
-
-          def protection_params
-              params.require(:protections).protections(:name, :permit_id)
+          if @protection.save
+            render json: @protection, status: :created
+          else
+            render json: @protection.errors, status: :unprocessable_entity
           end
         end
+
+        def update
+          @protection = Protection.find(params[:id])
+
+          if @protection.update(protection_params)
+            render json: @protection, status: :update
+          else
+            render json: @protection.errors, status: :unprocessable_entity
+          end
+        end
+
+        def destroy
+          @protection = Protection.find(params[:id])
+          if @protection
+            @protection.destroy
+          else
+            render json: {post: "not found"}, status: :not_found
+          end
+        end
+
+        private
+
+        def protection_params
+            params.require(:protection).protections(:name, :permit_id)
+        end
+      end
     end
 end
---
+++ spec/controllers/protections_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ProtectionsController, type: :controller do
+
+end
