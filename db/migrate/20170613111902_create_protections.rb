fd79765d59c23431e1244918a60780c83f4d5477
Danick <danick.sikkema@gmail.com>
2017-06-13 15:07:57 +0200

    update

--- db/migrate/20170613111902_create_protections.rb
+++ db/migrate/20170613111902_create_protections.rb
@@ -2,7 +2,7 @@ class CreateProtections < ActiveRecord::Migration[5.0]
   def change
     create_table :protections do |t|
       t.string :name
-      t.belongs_to :permit, foreign_key: true
+      t.belongs_to :permit, index: true
 
       t.timestamps
     end
